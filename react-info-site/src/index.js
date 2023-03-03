0ce4184d393310674773e53631429d7a91378608
Jakestep <jakestep.17@gmail.com>
2023-03-03 10:11:40 -0700

    3/3/23

---
+++ react-info-site/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import reactLogo from './images/react-logo.png';
+
+/**
+Challenge: fix our code!
+
+Don't forget, you're not using CDNs anymore, so there's no
+global "ReactDOM" variable to use anymore.
+ */
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const page = (
+  <div>
+    <img src={reactLogo} alt='react logo' width='40px'></img>
+    <h1>Fun Facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Facebook (now meta)</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+)
+
+root.render(page)
