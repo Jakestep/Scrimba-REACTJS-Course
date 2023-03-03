8985f1ef35d53f04c747032fbabd76881003c753
Jakestep <jakestep.17@gmail.com>
2023-03-03 10:49:23 -0700

    3/3/23 final

--- react-info-site/src/index.js
+++ react-info-site/src/index.js
@@ -13,18 +13,20 @@ global "ReactDOM" variable to use anymore.
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const page = (
-  <div>
-    <img src={reactLogo} alt='react logo' width='40px'></img>
-    <h1>Fun Facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on GitHub</li>
-      <li>Is maintained by Facebook (now meta)</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-)
+function Page() {
+  return (
+    <div>
+      <img src={reactLogo} alt='react logo' width='40px'></img>
+      <h1>Fun Facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook (now meta)</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  )
+}
 
-root.render(page)
+root.render(<Page />)
