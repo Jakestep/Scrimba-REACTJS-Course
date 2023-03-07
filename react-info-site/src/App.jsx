a3211c1c64ead1cbf156b513905c45ed00aef479
Jakestep <jakestep.17@gmail.com>
2023-03-07 11:32:21 -0700

    3/7/23 11:32

--- react-info-site/src/App.jsx
+++ react-info-site/src/App.jsx
@@ -3,13 +3,6 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import MainContent from './components/MainContent.jsx';
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import Page from './App';
-// import reportWebVitals from './reportWebVitals';
-
-
 
 
 function App() {
@@ -27,37 +20,6 @@ function App() {
         </div>
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
 
--- react-info-site/src/components/MainContent.jsx
+++ react-info-site/src/components/MainContent.jsx
@@ -5,8 +5,7 @@ function MainContent() {
         <div>
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
-                <li className='reasons'>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
+                <li className='reasons'>Reason 1</li>
                 <li className='reasons'>I'm more likely to get a job as a developer
                 if I know React</li>
             </ol>
