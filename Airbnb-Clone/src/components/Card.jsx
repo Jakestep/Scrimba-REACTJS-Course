2abc508e05226c90bd7003773110795d4978363c
Jakestep <jakestep.17@gmail.com>
2023-04-04 09:43:18 -0600

    okay now I'm actually moving on

--- Airbnb-Clone/src/components/Card.jsx
+++ Airbnb-Clone/src/components/Card.jsx
@@ -1,5 +1,6 @@
 export default function Card(props) {
-
+    console.log(props)
+    // console.log(props)
     let badgeText
     if(props.openSpots === 0) {
         badgeText = "SOLD OUT"
@@ -12,11 +13,11 @@ export default function Card(props) {
     return (
         <div className='card-block'>
             {badgeText && <div className='card--badge'>{badgeText}</div>}
-            <img src={`/assets/images/${props.img}`} className='card--image'/>
+            <img src={`/assets/images/${props.coverImg}`} className='card--image'/>
             <div>
                 <div className='card--stats-block'>
                     <img src='/assets/images/star.png' />
-                    <p className='card--stats'>{props.rating} <span className='light'>({props.reviewCount}) • {props.location}</span></p>
+                    <p className='card--stats'>{props.stats.rating} <span className='light'>({props.reviewCount}) • {props.location}</span></p>
                 </div>
                 <p className='card--title'>{props.title}</p>
                 <p className='card--price'><span className='bold'>From ${props.price}</span> / person</p>
