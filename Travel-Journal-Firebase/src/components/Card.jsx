bbc0d01242e7e2541c5aeea72ef454e14f606b72
Jakestep <jakestep.17@gmail.com>
2023-07-07 09:45:41 -0600

    asdf

--- Travel-Journal-Firebase/src/components/Card.jsx
+++ Travel-Journal-Firebase/src/components/Card.jsx
@@ -3,7 +3,9 @@ export default function Card(props) {
     return(
         <div className="card">
             <div className='imageHolder'>
-                <img src={props.image} className='cardPic'></img>
+                <a href='' target='_blank'>
+                    <img src={props.image} className='cardPic'></img>
+                </a>
             </div>
             <h2 className='cardTitle'>{props.title}</h2>
             <h3 className='cardDescription'>{props.description}</h3>
